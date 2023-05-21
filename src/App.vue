96213926753a23c7380795daae03588a46b2e44c
unknown <tanvirsiam1101@gmail.com>
2023-05-21 17:54:48 +0600

    binding styles partially done

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,22 @@
   <h2 v-text="channel"></h2>
   <p v-html="bhtml"></p>
   <h2 v-bind:id="headingID">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="italic" v-bind:class="status">Status</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Movie soldout?</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly promoted movies</h2>
+  <h2 v-bind:class="{
+    'promoted': isPromoted,
+    'new': !isSoldout,
+    'sold-out': isSoldout
+  }">Object conditional movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array Conditional Movies</h2>
+  <h2 v-bind:style="{
+    'color': fontColor,
+    'fontSize': headersize,
+  }">Inline Styling</h2>
+  <h2 v-bind:style="headerStyleObject">Header Style using object</h2>
 </template>
 
 <script>
@@ -17,6 +33,17 @@ export default {
       channel: 'Codevolution',
       bhtml: '<i>Calverin</i>',
       headingID: 'heading',
+      isDisabled: false,
+      status: "success",
+      isPromoted: true,
+      isSoldout: true,
+      fontColor: 'orange',
+      headersize: '50px',
+      headerStyleObject: {
+        color: 'orange',
+        fontSize: '50px',
+        textDecoration: 'italic',
+      }
     };
   }
 }
@@ -30,5 +57,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.success {
+  text-decoration: underline;
+}
+
+.italic {
+  color: blueviolet;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
 }
 </style>
